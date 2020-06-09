8a745d36b2cdf6f4c93d9de1af4ddefdf6e5fbcd
Jonathan Oliveira <jonathanh.oliveira@live.com>
2020-06-09 20:33:05 -0300

    Initial Commit - Create project and configure SignalRCore/Hub

---
+++ RealTime/RealTimeApi/Hubs/RealTimeHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealTimeApi.Models;
+using System.Threading.Tasks;
+
+namespace RealTimeApi.Hubs
+{
+    public class RealTimeHub : Hub
+    {
+
+        public async Task CadastrarPromocao(Promocao promocao)
+        {
+
+            await Clients.Caller.SendAsync("CadastradoSucesso");
+
+
+            await Clients.All.SendAsync("ReceberPromocao", promocao);
+        }
+
+
+    }
+}
